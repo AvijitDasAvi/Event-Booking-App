04f2e0e5eb8ccf30944bc9c92bb530f993379aca
Avijit Das <avijitavi338895@gmail.com>
2025-01-18 20:32:15 +0600

    Firebase connect with web

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,49 @@
 import 'package:event_booking_app/pages/sign_up_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+          apiKey: "AIzaSyBZSb01KEQFcYYdN3Ck3cUJtARPadqDPxE",
+          authDomain: "event-booking-app-6f69b.firebaseapp.com",
+          projectId: "event-booking-app-6f69b",
+          storageBucket: "event-booking-app-6f69b.firebasestorage.app",
+          messagingSenderId: "1069776236664",
+          appId: "1:1069776236664:web:5150bb094ce233a235fd8d",
+          measurementId: "G-TLJY2WTQK5"),
+    );
+  } else {
+    Firebase.initializeApp();
+  }
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Event Booking",
-      home: SignUpPage(),
+      title: 'Quiz-App',
+      home: FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            print("Error");
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            return SignUpPage();
+          }
+          return const CircularProgressIndicator();
+        },
+      ),
     );
   }
 }

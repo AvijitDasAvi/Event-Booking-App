26bc781d98978372d4b9231a053d4b3ce1c7dcdd
Avijit Das <avijitavi338895@gmail.com>
2025-01-28 20:51:52 +0600

    Added dependencies

--- lib/widgets/bottom_nav_bar.dart
+++ lib/widgets/bottom_nav_bar.dart
@@ -1,3 +1,6 @@
+import 'package:event_booking_app/pages/booking_page.dart';
+import 'package:event_booking_app/pages/home_page.dart';
+import 'package:event_booking_app/pages/profile_page.dart';
 import 'package:flutter/material.dart';
 
 class BottomNavBar extends StatefulWidget {
@@ -8,6 +11,19 @@ class BottomNavBar extends StatefulWidget {
 }
 
 class _BottomNavBarState extends State<BottomNavBar> {
+  late List<Widget> pages;
+  late HomePage homePage;
+  late BookingPage bookingPage;
+  late ProfilePage profilePage;
+  @override
+  void initState() {
+    homePage = HomePage();
+    bookingPage = BookingPage();
+    profilePage = ProfilePage();
+    pages = [homePage, bookingPage, profilePage];
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
